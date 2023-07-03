48cf0e9d02f4974d694e4d54195bc1beaf5faa9a
Molinari-Santiago <133693706+Molinari-Santiago@users.noreply.github.com>
2023-07-03 08:42:43 -0300

    Update moli bisiesto.cpp

--- moli bisiesto.cpp
+++ moli bisiesto.cpp
@@ -1,21 +1,21 @@
 #include<iostream>
 using namespace std;
-bool AnioDoblesiesta (int a);
+bool Aniobisiesto(int a);
 int main(){
 int n;
 cout<<"ingrese un anio"<<endl;
 cin>>n;
 
-if(AnioDoblesiesta ( n)==true){
+if(Aniobisiesto ( n)==true){
 	cout<<"No";
 }
-if(AnioDoblesiesta ( n)==false){
+if(Aniobisiesto ( n)==false){
 	cout<<"Si";
 }
 
 
 return 0;}
-bool AnioDoblesiesta (int a){
+bool Aniobisiesto(int a){
 bool flag=true;
 if(a%4==0){
 	flag=false;
